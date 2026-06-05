996d493e3d449da3fc9022e11781a2aed1a40905
zastecee <zastecee@gmail.com>
2026-06-05 10:02:51 +0200

    Final model

--- app.py
+++ app.py
@@ -62,13 +62,13 @@ from sklearn.model_selection import train_test_split
 train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
 
 # Define model
-melbourne_model_2 = DecisionTreeRegressor()
+melbourne_model = DecisionTreeRegressor(random_state=1)
 
 # Fit model
-melbourne_model_2.fit(train_X, train_y)
+melbourne_model.fit(train_X, train_y)
 
 # get predicted prices on validation data
-val_predictions = melbourne_model_2.predict(val_X)
+val_predictions = melbourne_model.predict(val_X)
 print("--- Mean Absolute Error In Validation Data ---")
 print(mean_absolute_error(val_y, val_predictions))
 
@@ -92,3 +92,28 @@ print(results.head())
 
 # print("First in-sample predictions:", iowa_model.predict(X.head()))
 # print("Actual target values for those homes:", y.head().tolist())
+
+# Underfitting and Overfitting
+def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    model.fit(train_X, train_y)
+    preds_val = model.predict(val_X)
+    mae = mean_absolute_error(val_y, preds_val)
+    return mae
+
+# compare MAE with differing values of max_leaf_nodes
+for max_leaf_nodes in [5, 50, 500, 5000]:
+    my_mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
+    print(f"Max leaf nodes: {max_leaf_nodes} \t\t Mean Absolute Error: {my_mae}")
+
+
+candidate_max_leaf_nodes = [5, 25, 50, 100, 250, 500]
+# Write loop to find the ideal tree size from candidate_max_leaf_nodes
+scores = { leaf_size: get_mae(leaf_size, train_X, val_X, train_y, val_y) for leaf_size in candidate_max_leaf_nodes }
+
+# Store the best value of max_leaf_nodes (it will be either 5, 25, 50, 100, 250 or 500)
+best_tree_size = min(scores, key=scores.get)
+
+# Fit Model Using All Data
+final_model = DecisionTreeRegressor(max_leaf_nodes=best_tree_size, random_state=1)
+final_model.fit(X, y)
